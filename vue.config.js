6bc78e55ba516920a3a1f436733052d8fbabf37e
dferna10 <36699584+dferna10@users.noreply.github.com>
2021-01-02 19:07:28 +0100

    Intento de reutas

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
 module.exports = {
     transpileDependencies: ["vuetify"],
     chainWebpack: config => {
-        config.module.rules.delete('eslint');
+        // config.module.rules.delete('eslint')
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
     },
     publicPath: process.env.NODE_ENV === 'production' ?
-        './' :
-        '/'
+        './' : '/'
 };
